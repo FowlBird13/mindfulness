342aa1c6261ecf9d3d6be882359a93aadff60a1d
Garyeong Bae <orisumin@gmail.com>
2026-06-12 13:05:35 -0600

    fixed syntax error

--- Reflecting.cs
+++ Reflecting.cs
@@ -6,7 +6,7 @@ public class Reflecting :Activity
         "Think of a time when you did something really difficult.",
         "Think of a time when you helped someone in need.",
         "Think of a time when you did something truly selfless."
-    }
+    };
     private List<string> _GBquestions = new List<string>
     {
         "Why was this experience meaningful to you?",
